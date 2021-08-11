91ced74a8a6ce0ae8d702b8740f2c110bd3ef341
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-08-11 11:29:41 +1000

    Create showiproute2csv_iterate.py

---
+++ netmikoLab/showiproute2csv_iterate.py
@@ -0,0 +1,44 @@
+from textfsm import TextFSM
+from netmiko import ConnectHandler
+from getpass import getpass
+from pandas import DataFrame
+from tabulate import tabulate
+from io import StringIO
+
+#Get login detais from User input
+USERNAME = input("Please enter username:")
+PASSWORD = getpass()
+
+#print("U: {}, P: {}".format(USERNAME, PASSWORD))
+#read device list from a file put in a list 'devices'
+
+#Read template from a file and store to template
+with open("showiproute.template", 'rt') as f:
+    template = f.read()
+
+#Read device list to put in a list devices
+with open("devices_file", 'rt') as f:
+    devices = f.read().splitlines()
+
+#Go through devices
+for device in devices:
+    device_details ={
+                    'device_type': 'cisco_ios',
+                    'ip': device,
+                    'username': USERNAME,
+                    'password': PASSWORD,
+                     }
+
+    conn = ConnectHandler(**device_details)
+    shiproute = conn.send_command('show ip route')
+    #print(shiproute)
+    print("--  ----------  ------  --------------  ------  ----------  --------  ------------  ------------------  --------")
+
+    parser = TextFSM(StringIO(template))
+    hdrs, vals = parser.header, parser.ParseText(shiproute)
+    df = DataFrame(vals, columns = hdrs)
+    print(tabulate(df, headers=hdrs))
+    df.to_csv("{}.csv".format(device))
+
+    print("--  ----------  ------  --------------  ------  ----------  --------  ------------  ------------------  --------")
+    print("--  ----------  ------  --------------  ------  ----------  --------  ------------  ------------------  --------")
