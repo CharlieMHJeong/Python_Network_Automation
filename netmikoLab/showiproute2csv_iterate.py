0810b3f31ec98105b0eeb1eeb7a2f063e0b3f3fd
CharlieMHJEong <41171446+CharlieMHJeong@users.noreply.github.com>
2021-08-11 11:44:10 +1000

    Update showiproute2csv_iterate.py

--- netmikoLab/showiproute2csv_iterate.py
+++ netmikoLab/showiproute2csv_iterate.py
@@ -8,19 +8,17 @@ from io import StringIO
 #Get login detais from User input
 USERNAME = input("Please enter username:")
 PASSWORD = getpass()
-
 #print("U: {}, P: {}".format(USERNAME, PASSWORD))
-#read device list from a file put in a list 'devices'
 
-#Read template from a file and store to template
+#Read template from a file and store to a var - template
 with open("showiproute.template", 'rt') as f:
     template = f.read()
 
-#Read device list to put in a list devices
+#Read device list to put in a list - devices
 with open("devices_file", 'rt') as f:
     devices = f.read().splitlines()
 
-#Go through devices
+#Go through devices 
 for device in devices:
     device_details ={
                     'device_type': 'cisco_ios',
